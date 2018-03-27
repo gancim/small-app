647c2a3908c3770d3f948762af9ea62d693e3e5a
Marco Ganci <marco.ganci@gmail.com>
2018-03-27 16:57:50 +0200

    port

--- small-app/src/main/java/com/mg/small/Main.java
+++ small-app/src/main/java/com/mg/small/Main.java
@@ -1,14 +1,11 @@
 package com.mg.small;
-import static spark.Spark.*;
+import static spark.Spark.get;
 
 import com.mg.small.obj.NumberPrevision;
-
-import spark.Spark;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Spark.port(8080);
 		// TODO Auto-generated method stub
 		get("/prevision", (req,res) -> {
 			NumberPrevision np = new NumberPrevision();
